40d9a361b6367f2764d303a00cc2729ed9034e2e
boopathygit <75171539+boopathygit@users.noreply.github.com>
2023-10-13 20:52:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ provider "aws" {
   region     = "ap-southeast-2"
 }
 resource "aws_instance" "web1" {
-ami           = "ami-06e2b86bab2edf4ee"
+#ami           = "ami-06e2b86bab2edf4ee"
+ami           = "ami-03131d9a8e6e8fad7"
 instance_type = "t2.micro"
 key_name   = "maternew"
 tags = {
