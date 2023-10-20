fe500650912e8e4b466f5325974cd66e005e6d17
boopathygit <75171539+boopathygit@users.noreply.github.com>
2023-10-20 17:28:19 +0530

    Create main.tf

---
+++ terraformwithvars/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region     = "ap-southeast-2"
+}
+resource "aws_instance" "web1" {
+#ami           = "ami-06e2b86bab2edf4ee"
+ami           = "ami-03131d9a8e6e8fad7"
+instance_type = "t2.micro"
+key_name   = "maternew"
+tags = {
+    Name = "Test-server-git"
+}
+}
+output "pvt_ip"{
+   value = aws_instance.web1.private_ip
+}
+output "pub_ip"{
+   value = aws_instance.web1.public_ip
+}
