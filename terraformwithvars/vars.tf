5d6f694a51490174dc51a64927d1bef804baaae0
boopathygit <75171539+boopathygit@users.noreply.github.com>
2023-10-23 23:17:16 +0530

    Update vars.tf

--- terraformwithvars/vars.tf
+++ terraformwithvars/vars.tf
@@ -2,3 +2,7 @@ variable "my_instance_type" {
    	type = string
       description = "Instance_Type in Jenkin/postman my_instance_type in terraform"
 }
+variable "my_ami" {
+   	type = string
+      description = "AMI in Jenkin/postman my_ami in terraform"
+}
