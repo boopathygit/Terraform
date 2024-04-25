0337831d012db38d8943cbf0767aaf2fcea06877
boopathygit <75171539+boopathygit@users.noreply.github.com>
2024-04-25 12:48:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "web1" {
 #ami           = "ami-06e2b86bab2edf4ee"
-ami           = "ami-03131d9a8e6e8fad7"
+ami           = "ami-04678842882445956"
 instance_type = "t2.micro"
 key_name   = "maternew"
 tags = {
